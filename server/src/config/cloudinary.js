56e9088d1ae2023381e6449869e36322694b73f9
Nazneen Begam <naaz@Mac.lan>
2026-07-29 22:54:00 +0530

    fix

--- server/src/config/cloudinary.js
+++ server/src/config/cloudinary.js
@@ -10,4 +10,9 @@ console.log({
   apiKeyLoaded: !!process.env.CLOUDINARY_API_KEY,
   apiSecretLoaded: !!process.env.CLOUDINARY_API_SECRET,
 });
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.api.ping()
+  .then((r) => console.log("Cloudinary Connected:", r))
+  .catch((e) => console.error("Cloudinary Error:", e));
 module.exports = cloudinary;
